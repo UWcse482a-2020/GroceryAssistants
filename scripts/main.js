eddfe774103a38f96d015dda788a9c62b95d88fe
lpjiang97 <prestonj@cs.washington.edu>
2020-06-05 16:04:11 -0700

    add autocomplete working need to add waiting note

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,9 @@ window.onload = function () {
     document.getElementById("update2ndQuant").addEventListener("click", this.updateSecondQuantity);
     document.getElementById("update3rdQuant").addEventListener("click", this.updateThirdQuantity);
     document.getElementById("update4thQuant").addEventListener("click", this.updateFourthQuantity);
-    searchForProductByNameBigList();
+    // searchForProductByNameBigList();
+    var testUniqueDescNames = ["Bread","Milk","Cheese", "Eggs", "Baby Formula"];
+    autocomplete(document.getElementById("product"), testUniqueDescNames);
 }
 
 function getLocation() {
@@ -86,15 +88,12 @@ async function searchForProductByUPC() {
     rankStores();
 }
 
-async function searchForProductByName() {
-    getLocation();
-    product = document.getElementById("product").value;
+async function searchForProductByName(query) {
     db = firebase.database();
     leadsRef = db.ref("/data");
     searchResults = [];
     searchResultKeys = [];
 
-    var query = product;
     var database = leadsRef.orderByChild("Description").startAt(query).endAt(query + "\uf8ff")
     var snapshot = await database.once('value');
 
@@ -104,7 +103,6 @@ async function searchForProductByName() {
             searchResultKeys.push(data.key);
         })
     }
-
     // get just descriptions
     var descNames = [];
     for (const d of searchResults) {
@@ -112,19 +110,116 @@ async function searchForProductByName() {
     }
     // ready to display in list herej
     let uniqueDescNames = [...new Set(descNames)]; 
+    console.log("got here");
 
-    if (uniqueDescNames.length == 0) {
-        alert("Sorry, no product was found.")
-        return ;
-    } 
-    // update product name
-    // var description = searchResults[0]["Description"]
-    // var size = searchResults[0]["Size"]
-    // var unit = searchResults[0]["Unit"]
-
-    // document.getElementById("foundProduct").innerText = description + " " + size + " " + unit;
-    // rankStores();
+    // if (uniqueDescNames.length == 0) {
+    //     alert("Sorry, no product was found.")
+    //     return ;
+    // } 
+    return uniqueDescNames;
 }
+
+function autocomplete(inp, arr) {
+    document.innerHTML = "Hello";
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", async function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        console.log(val);
+        arr = await searchForProductByName(val); 
+        console.log(arr);
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
 
 
 function rankStores() {
